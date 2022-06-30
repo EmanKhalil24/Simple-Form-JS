d038a1c46b24180528880de432884d1369c01b64
EmanKhalil24 <99218985+EmanKhalil24@users.noreply.github.com>
2022-06-30 15:05:01 +0200

    Add files via upload

---
+++ task7/simpleForm.js
@@ -0,0 +1,6 @@
+function info(){
+var username = document.getElementById("userName").value;
+var email = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+alert( `Your name is: ${username} ,Your email is: ${email} ,and Your Password is: ${password}` );
+}
